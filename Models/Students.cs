c50f3f21d201058353e0248192041a65a41b35a8
ramilaben1960 <51986248+ramilaben1960@users.noreply.github.com>
2019-06-19 01:22:33 -0600

    Create Students.cs

---
+++ Models/Students.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace WebAppication1.Models
+{
+    public class Students
+    {
+
+        public double AverageGrade { get; set; }
+
+        public List<StudentGradeDetail> StudentGradeDetail { get; set; }
+
+        public List<StudentGrade> ListOfStudets { get; set; }
+
+    }
+}
