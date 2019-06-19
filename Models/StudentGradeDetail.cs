1f9baacb9d32418ce48e29162756943b49e20ee1
ramilaben1960 <51986248+ramilaben1960@users.noreply.github.com>
2019-06-19 01:26:56 -0600

    Create StudentGradeDetail.cs

---
+++ Models/StudentGradeDetail.cs
@@ -0,0 +1,12 @@
+using System;
+namespace WebAppication1.Models
+{
+    public class StudentGradeDetail
+    {
+
+        public string LetterGread { get; set; }
+
+        public int NumberOfStudents { get; set; }
+
+    }
+}
