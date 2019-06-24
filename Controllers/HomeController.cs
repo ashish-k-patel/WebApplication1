d2c339ee034f45e2edcc7a42ce9662b7a42cd058
ramilaben1960 <51986248+ramilaben1960@users.noreply.github.com>
2019-06-23 21:39:26 -0600

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,12 +1,12 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using CodingTest.Models;
+using WebApplication1.Models;
 
-namespace CodingTest.Controllers
+namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
